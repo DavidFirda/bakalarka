0d5d6227aa466edbe98c166ff95a1f1cbc694a8c
DavidFirda <162358662+DavidFirda@users.noreply.github.com>
2025-05-27 10:36:03 +0200

    Delete RandomQuestionSelector.py

--- RandomQuestionSelector.py
+++
@@ -1,9 +0,0 @@
-import random
-
-class RandomQuestionSelector:
-    def __init__(self, categories, weak_categories):
-        self.categories = list(categories)
-        self.weak_categories = list(weak_categories)
-
-    def select_random_question(self):
-        return random.choice(self.categories)
